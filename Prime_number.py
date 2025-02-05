3279dafb0d495ad944fd0ba95eff28eaabb5dd3e
Swagat Das <swagat919@gmail.com>
2025-02-06 00:09:18 +0530

    Add files via upload

---
+++ Prime_number.py
@@ -0,0 +1,15 @@
+number = int(input("Enter a number to check if it's prime: "))
+is_prime = True
+
+if number <= 1:
+    is_prime = False
+else:
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
